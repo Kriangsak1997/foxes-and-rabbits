01a0a6970619b1eb376ee1cc281ef17cff163fe9
Kriangsak1997 <kriangsak.thi@student.mahidol.edu>
2021-07-14 19:10:05 +0700

    tiger added

--- src/main/java/io/muic/ooc/fab/AnimalType.java
+++ src/main/java/io/muic/ooc/fab/AnimalType.java
@@ -1,10 +1,16 @@
 package io.muic.ooc.fab;
 
+import java.awt.*;
+import java.util.Collections;
+
 public enum AnimalType {
-    RABBIT(0.08, Rabbit.class),
-    FOX(0.02, Fox.class);
+    RABBIT(0.08, Rabbit.class, Color.ORANGE),
+    FOX(0.02, Fox.class, Color.BLUE),
+    TIGER(0.04, Tiger.class, Color.RED);
 
     private double breedingProbability;
+    private Color color;
+
 
 
     private Class animalClass;
@@ -17,8 +23,13 @@ public enum AnimalType {
         return breedingProbability;
     }
 
-    AnimalType(double breedingProbability, Class animalClass) {
+    public Color getColor() {
+        return color;
+    }
+
+    AnimalType(double breedingProbability, Class animalClass, Color color) {
         this.breedingProbability = breedingProbability;
         this.animalClass = animalClass;
+        this.color = color;
     }
 }
